d1550181eb7458eba088edada385d932fa905ec2
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-11 12:01:41 +0300

    Theme Color schema

---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+export const theme = {
+  colors: {
+    primary: "#001900",
+    secondary: "#ff0000",
+    background: "#ffffff",
+    paper: "#f5f5f5",
+  },
+  fontsSizes: {
+    small: "1.2rem",
+    medium: "1.6rem",
+    large: "2.4rem",
+    xlarge: "3.2rem",
+  },
+  fonts: {
+    primary: "Roboto, sans-serif",
+  },
+  media: {
+    mobile: "(max-width: 768px)",
+    tablet: "(max-width: 1024px)",
+    desktop: "(min-width: 1025px)",
+  },
+};
