b81693e23206c3545478298f9d7601be09c15e2e
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-11 12:27:05 +0300

    Style for signup using styled component

---
+++ src/components/pages/Signup/style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledSignIn = styled.div`
+  .login_with_exist_account {
+    margin: 0 auto;
+    display: block;
+    text-align: center;
+    margin-top: 20px;
+    a {
+      text-decoration: none;
+      margin-left: 5px;
+      color: #319325;
+    }
+  }
+  .signup_form {
+    width: "100%";
+  }
+`;
