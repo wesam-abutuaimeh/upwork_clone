cdba6184a1568ca92aec1636e943fd10ad973d1d
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-12 13:38:13 +0300

    Login schema

--- src/validation/authSchema.ts
+++ src/validation/authSchema.ts
@@ -14,7 +14,16 @@ export const registrationSchema = Yup.object().shape({
       "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
     )
     .required("Password is required"),
-  agreement: Yup.boolean()
-    .oneOf([true], "You must agree to our Policy")
-    .required("You must agree to our Policy"),
+});
+
+export const loginSchema = Yup.object().shape({
+  email: Yup.string()
+    .matches(
+      /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+      "Invalid Email"
+    )
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
 });
