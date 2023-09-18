d327459fa373b75a485c25ce9858f79cace135db
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-18 18:20:07 +0300

    Avatar component

---
+++ src/components/atoms/Avatar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Avatar } from "@mui/material";
+
+const index = () => {
+  return (
+    <Avatar
+      alt="Remy Sharp"
+      src="/upwork_favicon.jpeg"
+      sx={{ width: 50, height: 50 }}
+    />
+  );
+};
+
+export default index;
