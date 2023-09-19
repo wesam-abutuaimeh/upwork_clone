9435cc4ae1af502cdc6b4fa8dbcca28bb8acaf44
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-19 16:13:26 +0300

    Make Dynamic props for CustomLink Component

--- src/components/atoms/CustomLink/index.tsx
+++ src/components/atoms/CustomLink/index.tsx
@@ -1,13 +1,4 @@
 import React from "react";
 import Link from "next/link";
-
-interface CustomLinkProps {
-  href: string;
-  children: React.ReactNode;
-}
-
-const CustomLink: React.FC<CustomLinkProps> = ({ href, children }) => (
-  <Link href={href}>{children}</Link>
-);
-
+const CustomLink = (porps: any) => <Link {...porps}>{porps.children}</Link>
 export default CustomLink;
