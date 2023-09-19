8ba749f4e7d3e42b9095d2946e9e71abfc41e82e
Wesam-abutuaimeh <wesam@ucst.edu.ps>
2023-09-19 18:03:53 +0300

    comman style

---
+++ src/styles/commen.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledAside = styled.aside`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
+  gap: 10px;
+  padding: 10px;
+  margin-left: 10px;
+  border: 1px solid #d5e0d5;
+  border-radius: 11px;
+`;
